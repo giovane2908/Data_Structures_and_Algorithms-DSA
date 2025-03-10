f4dd3fec54fc70a9a90aa019a32c28b401c94eea
giovane2908 <giovanetrevisan2908@gmail.com>
2025-03-10 15:07:25 -0300

    Update Binary_Search.py

---
+++ Arrays/Binary_Search.py
@@ -0,0 +1,24 @@
+def binary_search(nums, n):
+    left = 0 
+    right = len(nums)  
+    steps = 0
+
+    while left < right:
+        steps += 1
+        mid = int((left + right) / 2)
+
+        if nums[mid] == n:
+            print(f"Found {n} in {steps} steps")
+            return mid
+        elif nums[mid] < n:
+            left = mid + 1
+        else:
+            right = mid
+
+    return -1
+array_len = int(input("Digite o tamanho do array: "))
+
+array_len = [i for i in range(1, array_len+1)]
+
+binary_search(array_len, 3)
+
