1c1f447d289f2b315ac4d901ea308e9849d8284a
giovane2908 <giovanetrevisan2908@gmail.com>
2025-03-10 14:23:53 -0300

    Update Two_Pointer.py

--- Arrays/Two_Pointer.py
+++ Arrays/Two_Pointer.py
@@ -5,18 +5,18 @@ def reverseWords(string):
     l, r = 0, 0 #Ponteiros para o inicio e fim da palavra
 
     while r < len(string): 
-        if string[r] != '':
+        if string[r] != '': #move o ponteiro r ate um espaco em branco ou seja o fim da palavra
             r += 1
         else:
-            res += string[l:r+1][::-1]
+            res += string[l:r+1][::-1] #adiciona a palavra invertida a string de res
             r += 1
             l = r
 
     res += ' '
-    res += string[l:r][::-1]
+    res += string[l:r][::-1] #iverter a ultima palavra
     return res[1:]
 
 
-input_string = "the sky is blue   dsaa  sda a a sdda da"
+input_string = "hello world"
 result = reverseWords(input_string)
 print(result)
